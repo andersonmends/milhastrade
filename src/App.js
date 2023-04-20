df4435daa4236fa565f4ede9607c69e9aee96f2f
Anderson Mendes <anderson.mends@gmail.com>
2023-04-19 22:31:07 -0300

    subtille

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
 
     <div>
 
-      <Navbar bg='primary' variant='dark' expand="lg" className='pl-1' >
+      <Navbar bg='primary' variant='dark' expand="lg" className='pl-1'>
         <Container className=''>
           <Navbar.Brand href="#home"> Milhastrade</Navbar.Brand>
           <Navbar.Toggle className='navbar-dark' aria-controls="basic-navbar-nav" />
@@ -67,8 +67,14 @@ function App() {
               <Nav.Link href="#link">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+
         </Container>
       </Navbar>
+      <Navbar className='justify-content-center'>
+        <small>Acompanhe a cotação de milhas com o histórico de preços e decida o momento certo de comprar e vender milhas
+        </small></Navbar>
+
+
 
       <AdsterraBannerWidget  ></AdsterraBannerWidget  >
 
@@ -79,6 +85,7 @@ function App() {
               <Card.Body >
 
                 <Card.Title className="h-100">Hotmilhas</Card.Title>
+                <Card.Subtitle className='text-success mt-1 mb-1'> R$10.00 <i class="fa fa-caret-up"></i>1,05%</Card.Subtitle>
                 <Button size="sm" style={{ outline: daysHotmilhas === 7 ? "1px solid #ddd  " : "none", margin: "0px 5px 0px 20px" }} variant="outline-secondary" onClick={() => setDaysHotmilhas(7)}>7D</Button>
                 <Button size="sm" style={{ outline: daysHotmilhas === 30 ? "1px solid #ddd  " : "none", margin: "0 5px" }} variant="outline-secondary" onClick={() => setDaysHotmilhas(30)}>1M</Button>
                 <Button size="sm" style={{ outline: daysHotmilhas === 180 ? "1px solid #ddd  " : "none", margin: "0 5px" }} variant="outline-secondary" onClick={() => setDaysHotmilhas(180)}>6M</Button>
@@ -90,33 +97,34 @@ function App() {
                       <Chart days={daysHotmilhas} color={colorCiaHotmilhas} data={hmData} dataXAxis={"date"} dataYAxis={nameCiaHotmilhas} />
                     </div>
                   </Col>
-                  <Col sm={1}> 
+                  <Col sm={1}>
                     <Row >
-                      <Col className="d-flex" ><Card.Subtitle className='text-success'> R$10.00 <i class="fa fa-caret-down"></i>1,05%</Card.Subtitle></Col>
-                      
+                      {/* <Col className="d-flex" ><Card.Subtitle className='text-success'> R$10.00 <i class="fa fa-caret-down"></i>1,05%</Card.Subtitle></Col> */}
+
                       {/* <Col className="d-flex  justify-content-center" ><Card.Text></Card.Text></Col> */}
-                  </Row>
-                    
+                    </Row>
+
                   </Col>
                 </Row>
-               
-                
+
+
                 <ButtonCia selectedCia={selectedCiaHotmilhas} logo={smilesLogo} cia="Smiles" handle={() => handleHotmilhasClick("Gol", monaLisaGol, "Smiles")}></ButtonCia>
                 <ButtonCia selectedCia={selectedCiaHotmilhas} logo={tudoAzulLogo} cia="Tudoazul" handle={() => handleHotmilhasClick("Azul", fanBlueAzul, "Tudoazul")}></ButtonCia>
                 <ButtonCia selectedCia={selectedCiaHotmilhas} logo={latampassLogo} cia="Latampass" handle={() => handleHotmilhasClick("Latam", clairvoyantLatam, "Latampass")}></ButtonCia>
 
               </Card.Body>
-              
+
 
             </Card>
           </Col>
 
-          
+
 
           <Col>
             <Card className="mt-2">
               <Card.Body >
                 <Card.Title className="h-100">Maxmilhas</Card.Title>
+                <Card.Subtitle className='text-danger mt-1 mb-1'> R$15.00 <i class="fa fa-caret-down"></i>1,25%</Card.Subtitle>
                 <Button size="sm" style={{ outline: daysMaxmilhas === 7 ? "1px solid #ddd" : "none", margin: "0px 5px 0px 20px" }} variant="outline-secondary" onClick={() => setDaysMaxmilhas(7)}>7D</Button>
                 <Button size="sm" style={{ outline: daysMaxmilhas === 30 ? "1px solid #ddd" : "none", margin: "0 5px" }} variant="outline-secondary" onClick={() => setDaysMaxmilhas(30)}>1M</Button>
                 <Button size="sm" style={{ outline: daysMaxmilhas === 180 ? "1px solid #ddd" : "none", margin: "0 5px" }} variant="outline-secondary" onClick={() => setDaysMaxmilhas(180)}>6M</Button>
@@ -135,7 +143,7 @@ function App() {
         </Row>
 
         <AdsterraNativeBannerWidget />
-       
+
 
         <h2 className="mt-2" id="noticias">Notícias</h2>
         {newsData.map((news, index) => (
@@ -176,7 +184,7 @@ function App() {
                 </a>
                 <a href="#" target="_blank">
                   <i className="fab fa-github pr-3 text-white" />
-                  
+
                 </a>
               </div>
             </div>
