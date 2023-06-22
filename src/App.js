6d1ca192d0a4c7900cd364e9e897d12463334f58
Anderson Mendes <anderson.mends@gmail.com>
2023-06-21 22:17:01 -0300

    try rss feed

--- src/App.js
+++ src/App.js
@@ -9,19 +9,72 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { NewsPage } from './views/pages/NewsPage';
 import { ScrollToTop } from './views/components/ScrollToTop';
 import { Analytics } from '@vercel/analytics/react';
-import * as Realm from "realm-web";
-import { AdscashBannerWidget } from "./views/components/AdscashBannerWidget";
-// import { hmData, maxData } from "./assets/data";
+import Parser from 'rss-parser/dist/rss-parser';
+import cheerio from 'cheerio';
 
+
+async function fetchRSSFeed() {
+  const parser = new Parser();
+  const feed = await parser.parseURL('https://cors-anywhere.herokuapp.com/https://www.melhorescartoes.com.br/feed');
+  return feed.items.map(item => ({
+    ...item,
+    content: item['content:encoded'] ? item['content:encoded'] : item.content,
+  }));
+}
+
+
+function extractFirstImage(content) {
+  const doc = new DOMParser().parseFromString(content, 'text/html');
+  const imgElement = doc.querySelector('img');
+  return imgElement ? imgElement.src : null;
+}
+
+function extractContentWithoutImages(content) {
+  const doc = new DOMParser().parseFromString(content, 'text/html');
+  const imgElements = doc.querySelectorAll('img');
+  imgElements.forEach(img => img.remove());
+  return doc.body.innerHTML;
+}
 
 function App() {
+
+  const [rssItems, setRssItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const items = await fetchRSSFeed();
+      setRssItems(items);
+    }
+
+    fetchData();
+  }, []);
+
+ 
     
   return (
     <>
       <Header />
-{/* <AdscashBannerWidget/> */}
+      <div>
+        
+        <ul>
+          {rssItems.map((item, index) => {
+            const firstImage = extractFirstImage(item.content);
+            const contentWithoutImages = extractContentWithoutImages(item.content);
 
-      <BrowserRouter>
+            return (
+              <li key={index}>
+                <a href={item.link}>{item.title}</a>
+                {firstImage && <img src={firstImage} alt="First Image" />}
+                <p>{item.contentSnippet}</p>
+                <p dangerouslySetInnerHTML={{ __html: contentWithoutImages }}></p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+
+      {/* <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home/>} />
@@ -30,7 +83,7 @@ function App() {
           <Route path="/programas" element={<Programas/>} />
           <Route path="/news/:id" element={<NewsPage/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
 
       <Footer></Footer>
       <Analytics />
